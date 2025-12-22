a9241a1afee5f69f7b3e529830d638d4e8764d10
Tiago Lopes <tiago.lopes.comercial@gmail.com>
2025-12-22 15:14:58 -0300

    Melhorada extracao de dados, corrigido problema com valores vazios e duplicados

--- extracaoDadosDiretorio.py
+++ extracaoDadosDiretorio.py
@@ -52,20 +52,26 @@ def extrairDadosDiretorio(lattes_id: str) -> None:
             lista_cabecalhos_ignorar = ['IOEPEMC', 'ui-button', 'Ações']
             for cabecalho in cabecalhos:
                 texto = cabecalho.text
+                if texto == 'Ações': break
                 if texto in lista_cabecalhos_ignorar : continue
                 if texto:
                     lista_cabecalhos.append(texto)
 
             valores = element.select('td')
             lista_valores = []
-            lista_valores_ignorar = ['Nenhum registro adicionado']
+            lista_valores_ignorar = ['Nenhum registro adicionado', 'ui-buttonVisualizar']
             for valor in valores:
                 texto = valor.text
+                texto = texto.strip()
                 if texto.startswith('Visualizar espelho'): continue
-                if texto in lista_valores_ignorar: continue
+                pular = False
+                for ignorar in lista_valores_ignorar:
+                    if ignorar in texto: 
+                        pular = True
+                        break
 
-                if valor.text:
-                    lista_valores.append(valor.text)
+                if pular: continue
+                lista_valores.append(texto)
 
             contador = 0
             titulo = titulo.replace(' ', '_').upper()
@@ -75,7 +81,8 @@ def extrairDadosDiretorio(lattes_id: str) -> None:
                 cabecalho = lista_cabecalhos[contador]
                 dicionario = {}
                 dicionario[cabecalho] = valor
-                escreverCSV(caminho, lattes_id, dicionario=dicionario)
+                if valor:
+                    escreverCSV(caminho, lattes_id, dicionario=dicionario)
                 contador += 1
 
     else:
@@ -86,4 +93,39 @@ def extrairDadosDiretorio(lattes_id: str) -> None:
 if __name__ == '__main__':
     lattes_id=2000279230405792 # SERGIO SAN GREGORIO FAVERO
     lattes_id=3065759072608048 # ALEXANDRE ANDRADE DOS ANJOS JACOME
-    extrairDadosDiretorio(lattes_id)
+    lista = [
+    "2558358124098455",
+    "9208985005837277",
+    "1272058768233040",
+    "3329079748927880",
+    "0475167659981163",
+    "9978224480666395",
+    "2633867419420883",
+    "7717444217239525",
+    "4827680511742964",
+    "7300981629114346",
+    "1929897521905456",
+    "7581141843974703",
+    "6208433886573740",
+    "8637319515539535",
+    "0602933124301833",
+    "2444841881701549",
+    "7170570436557463",
+    "7686691707525007",
+    "2194175558834284",
+    "2069769466957361",
+    "5330490614722659",
+    "6182477842336805",
+    "2618349568972136",
+    "5204063085335824",
+    "3050716643641697",
+    "5624554983752801",
+    "2381161563121871",
+    "5754413873836451",
+    "4636891232412658",
+    "7587451351635053",
+    "3615728985559494"
+]
+
+    for l in lista:
+        extrairDadosDiretorio(l)
